adc815605e5125bbf93d14ab643823b8cfaa7374
wenming0310 <474196608@qq.com>
2018-01-07 22:00:30 +0800

    2018年1月7日22:00:27

---
+++ code/PythonTest/Django/HelloWorld/HelloWorld/view.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+from django.http import HttpResponse
+
+def hello(request):
+    return HttpResponse("Hello world !")
+'''
+from django.shortcuts import render
+
+def hello(request):
+    context           = {}
+    context['hello'] = 'Hello World!'
+    context['apple'] = 'Hello World!'
+    return render(request, 'hello.html', context)
--- code/PythonTest/大数据/pic.py
+++ code/PythonTest/大数据/pic.py
@@ -10,14 +10,14 @@ import random   # 随机生成一个实数，他的范围[0,1]内
 
 def spiderPic(html, keyword):
     print('正在查找：' + keyword + '对应的图像，正在从百度图片中下载文件，请稍等...')
-    for addr in re.findall('"objURL":".*?"', html, re.s):
+    for addr in re.findall('"objURL":"(.*?)"', html, re.S):
         print('现在正在爬取URL地址：' + str(addr)[0:30] + "...")
         try:
-            pics = requests.get(addr, timeout=10) # 请求URL地址（最大时间10s）
+            pics = requests.get(addr, timeout=10) # 请求URL地址（最大时间(超时时间)10s）
         except requests.exceptions.ConnectionError:
             print('您当前的URL地址请求错误！')
             continue
-        fq = open('E:\\pic\\' + (keyword + '_' +  str(random.randrange(0,1000,4))) + '.jpg')
+        fq = open('E:\\pic\\' + (keyword + '_' +  str(random.randrange(0,1000,4)) + '.jpg'), 'wb')
         fq.write(pics.content)
         fq.close()
 
@@ -25,7 +25,7 @@ def spiderPic(html, keyword):
 # python 的主方法
 if __name__ == '__main__':
     word = input('请输入你想要爬取的图像的关键词：')
-    requests = requests.get('https://image.baidu.com/search/index?tn=baiduimage&ct=201326592&lm=-1&cl=2&ie=gbk&word=%C3%C0%C5%AE&fr=ala&ala=1&alatpl=adress&pos=0&hs=2&xthttps=111111' +  spiderPic('', word))
+    #results = requests.get('https://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word=' +  word)
+    results = requests.get('https://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word=' + word)
 # 调用函数
-
-spiderPic('', word)
+spiderPic(results.text, word)
