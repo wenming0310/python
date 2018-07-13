0f7006b694286255588cca13c113510c9b44924d
zhaowenming <474196608@qq.com>
2018-07-13 19:36:56 +0800

    add play mp3 procee
    添加播放MP3音乐的模块，但是还没找到好用的

--- code/PythonTest/baidu-aip/baiduaiptest.py
+++ code/PythonTest/baidu-aip/baiduaiptest.py
@@ -1,4 +1,8 @@
 from aip import AipSpeech
+import time
+import pygame
+import os
+#import mp3play     #语速正常，但只能用于python2.7，不支持python3.x
 
 """ 你的 APPID AK SK """
 APP_ID = ''
@@ -13,3 +17,21 @@ if not isinstance(result, dict):
     with open('auido.mp3', 'wb') as f:
         f.write(result)
 
+file=r'D:\CC++C#Code\github\python\code\PythonTest\baidu-aip\auido.mp3'
+pygame.mixer.init() # 语速失真
+print("播放音乐1")
+track = pygame.mixer.music.load(file)
+pygame.mixer.music.play()
+time.sleep(10)
+pygame.mixer.music.stop()
+
+os.system('auido.mp3') #语速正常，缺点，弹出播放器窗口，打开完成后不占用进程
+'''
+def playmusic(path):
+    clip = mp3play.load(path)
+    clip.play()
+    time.sleep(10)
+    clip.stop()
+
+playmusic('auido.mp3')
+'''
