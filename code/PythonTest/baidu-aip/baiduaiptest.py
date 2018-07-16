dcb2f5358f1169f8eddefb8c01d0f78d9eaf32e7
zhaowenming <474196608@qq.com>
2018-07-16 19:10:32 +0800

    1

--- code/PythonTest/baidu-aip/baiduaiptest.py
+++ code/PythonTest/baidu-aip/baiduaiptest.py
@@ -1,13 +1,12 @@
 from aip import AipSpeech
 import time
-import pygame
-import os
+
 #import mp3play     #语速正常，但只能用于python2.7，不支持python3.x
 
 """ 你的 APPID AK SK """
-APP_ID = ''
-API_KEY = ''
-SECRET_KEY = ''
+APP_ID = '11529400'
+API_KEY = 't8siWxdXW9MpBkoSDSq953HW'
+SECRET_KEY = 'ghxa9CXHtfA3T9ZkM3TyGBfqSjq80KsG'
 
 client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
 
@@ -17,6 +16,7 @@ if not isinstance(result, dict):
     with open('auido.mp3', 'wb') as f:
         f.write(result)
 
+import pygame
 file=r'D:\CC++C#Code\github\python\code\PythonTest\baidu-aip\auido.mp3'
 pygame.mixer.init() # 语速失真
 print("播放音乐1")
@@ -25,7 +25,8 @@ pygame.mixer.music.play()
 time.sleep(10)
 pygame.mixer.music.stop()
 
-os.system('auido.mp3') #语速正常，缺点，弹出播放器窗口，打开完成后不占用进程
+#import os
+#os.system('auido.mp3') #语速正常，缺点，弹出播放器窗口，打开完成后不占用进程
 '''
 def playmusic(path):
     clip = mp3play.load(path)
@@ -35,3 +36,13 @@ def playmusic(path):
 
 playmusic('auido.mp3')
 '''
+
+import eyed3
+audiofile = eyed3.load("auido.mp3")
+#audiofile.tag.artist = u"Nobunny"
+#audiofile.tag.album = u"Love Visions"
+#audiofile.tag.album_artist = u"Various Artists"
+#audiofile.tag.title = u"I Am a Girlfriend"
+#audiofile.tag.track_num = 4
+#audiofile.tag.save()
+audiofile.
