41633e34f56ca4c8668c71cb8aedfe084c48b685
zhaowenming <474196608@qq.com>
2018-08-31 09:44:52 +0800

    1

--- code/PillowTest/画小猪佩奇_字符.py
+++ code/PillowTest/画小猪佩奇_字符.py
@@ -29,7 +29,8 @@ from PIL import Image
 #要索引的字符列表
 ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
 length = len(ascii_char)
-img = Image.open('小猪佩奇.jpg')      #读取图像文件
+#img = Image.open('小猪佩奇.jpg')      #读取图像文件
+img = Image.open('图形1000x1000.png')      #读取图像文件
 (width,height) = img.size
 img = img.resize((int(width*0.5),int(height*0.2)))  #对图像进行一定缩小,图片的大小可以根据上传图片的大小来按需调节，
 print(img.size)
