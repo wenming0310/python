ce53bff63506a4411c9f65d6fa500c2c747c5e11
zhaowenming <474196608@qq.com>
2018-09-03 19:04:10 +0800

    1

---
+++ code/PythonTest/数据类型/list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+list1 = ['Google', 'Runoob', 1997, 2000];
+list2 = [1, 2, 3, 4, 5, 6, 7];
+
+print("list1[0]: ", list1[0])
+print("list2[1:5]: ", list2[1:5])
+
+for I ,a in ((1,'a'),(3,'c'),(2,'b')):
+    print(I,a)
