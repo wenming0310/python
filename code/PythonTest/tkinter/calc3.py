1e8f6f56b8554ea8baa3529554819bde5ea16168
zhaowenming <474196608@qq.com>
2018-09-06 16:42:43 +0800

    1

--- code/PythonTest/tkinter/calc3.py
+++ code/PythonTest/tkinter/calc3.py
@@ -1,9 +1,9 @@
 from tkinter import *
-import Pmw
+import Pmw, string
 
 class SLabel(Frame):
-    """SLabel defines a 2-sided label within a Frame. The
-    left hand label has blue letters the right has white letters"""
+    """ SLabel defines a 2-sided label within a Frame. The
+        left hand label has blue letters the right has white letters """
 
     def __init__(self, master, leftl, rightl):
         Frame.__init__(self, master, bg='gray40')
@@ -23,7 +23,7 @@ class Key(Button):
         kw['fg'] = fg
         kw['width'] = width
         kw['borderwidth'] = borderwidth
-        #apply(Button.__init__(self, master), kw)
+
         Button.__init__(*(self, master), **kw)
         self.pack(side=LEFT, expand=NO, fill=NONE)
 
@@ -33,17 +33,21 @@ class Calculator(Frame):
         Frame.__init__(self, bg='gray40')
         self.pack(expand=YES, fill=BOTH)
         self.master.title('Tkinter Toolkit TT-42')
-        self.master.iconname('TK-42')
-        self.calc = Evaluator()
-        self.buildCalculator()
-
-        self.actionDict = {'second': self.doThis, 'mode': self.doThis, 'delete': self.doThis, 'alpha': self.doThis,
+        self.master.iconname('Tk-42')
+        self.calc = Evaluator()  # This is our evaluator
+        self.buildCalculator()  # Build the widgets
+        # This is an incomplete dictionary - a good exercise!
+        self.actionDict = {'second': self.doThis, 'mode': self.doThis,
+                           'delete': self.doThis, 'alpha': self.doThis,
                            'stat': self.doThis, 'math': self.doThis,
-                           'matrix': self.doThis, 'program': self.doThis, 'vars': self.doThis, 'clear': self.clearall,
+                           'matrix': self.doThis, 'program': self.doThis,
+                           'vars': self.doThis, 'clear': self.clearall,
                            'sin': self.doThis, 'cos': self.doThis,
-                           'tan': self.doThis, 'up': self.doThis, 'X1': self.doThis, 'X2': self.doThis,
-                           'log': self.doThis, 'ln': self.doThis, 'store': self.doThis,
-                           'off': self.turnoff, 'neg': self.doThis, 'enter': self.doEnter,
+                           'tan': self.doThis, 'up': self.doThis,
+                           'X1': self.doThis, 'X2': self.doThis,
+                           'log': self.doThis, 'ln': self.doThis,
+                           'store': self.doThis, 'off': self.turnoff,
+                           'neg': self.doThis, 'enter': self.doEnter,
                            }
         self.current = ""
 
@@ -82,54 +86,53 @@ class Calculator(Frame):
             pass
 
     def buildCalculator(self):
-        FUN = 1
-        KEY = 0
-        KC1 = 'gray30'
-        KC2 = 'gray50'
-        KC3 = 'steelblue1'
-        KC4 = 'steelblue'
+        FUN = 1  # Designates a Function
+        KEY = 0  # Designates a Key
+        KC1 = 'gray30'  # Dark Keys
+        KC2 = 'gray50'  # Light Keys
+        KC3 = 'steelblue1'  # Light Blue Key
+        KC4 = 'steelblue'  # Dark Blue Key
         keys = [
-            [('2nd', '', '', KC3, FUN, 'second'),
+            [('2nd', '', '', KC3, FUN, 'second'),  # Row 1
              ('Mode', 'Quit', '', KC1, FUN, 'mode'),
              ('Del', 'Ins', '', KC1, FUN, 'delete'),
              ('Alpha', 'Lock', '', KC2, FUN, 'alpha'),
              ('Stat', 'List', '', KC1, FUN, 'stat')],
-            [('Math', 'Test', 'A', KC1, FUN, 'math'),
+            [('Math', 'Test', 'A', KC1, FUN, 'math'),  # Row 2
              ('Mtrx', 'Angle', 'B', KC1, FUN, 'matrix'),
              ('Prgm', 'Draw', 'C', KC1, FUN, 'program'),
              ('Vars', 'YVars', '', KC1, FUN, 'vars'),
              ('Clr', '', '', KC1, FUN, 'clear')],
-            [('X-1', 'Abs', 'D', KC1, FUN, 'X1'),
+            [('X-1', 'Abs', 'D', KC1, FUN, 'X1'),  # Row 3
              ('Sin', 'Sin-1', 'E', KC1, FUN, 'sin'),
              ('Cos', 'Cos-1', 'F', KC1, FUN, 'cos'),
              ('Tan', 'Tan-1', 'G', KC1, FUN, 'tan'),
              ('^', 'PI', 'H', KC1, FUN, 'up')],
-            [('X2', 'Root', 'I', KC1, FUN, 'X2'),  # Row4
+            [('X2', 'Root', 'I', KC1, FUN, 'X2'),  # Row 4
              (',', 'EE', 'J', KC1, KEY, ','),
              ('(', '{', 'K', KC1, KEY, '('),
              (')', '}', 'L', KC1, KEY, ')'),
-             ('/', '', 'M', KC4, FUN, '/')],
-            [('Log', '10x', 'N', KC1, FUN, 'log'),  # Row5
+             ('/', '', 'M', KC4, KEY, '/')],
+            [('Log', '10x', 'N', KC1, FUN, 'log'),  # Row 5
              ('7', 'Un-1', 'O', KC2, KEY, '7'),
              ('8', 'Vn-1', 'P', KC2, KEY, '8'),
              ('9', 'n', 'Q', KC2, KEY, '9'),
              ('X', '[', 'R', KC4, KEY, '*')],
-            [('Ln', 'ex', 'S', KC1, FUN, 'ln'),  # Row6
+            [('Ln', 'ex', 'S', KC1, FUN, 'ln'),  # Row 6
              ('4', 'L4', 'T', KC2, KEY, '4'),
              ('5', 'L5', 'U', KC2, KEY, '5'),
              ('6', 'L6', 'V', KC2, KEY, '6'),
-             ('-', ')', 'W', KC4, KEY, '-')],
-            [('STO', 'RCL', 'X', KC1, FUN, 'store'),  # Row7
+             ('-', ']', 'W', KC4, KEY, '-')],
+            [('STO', 'RCL', 'X', KC1, FUN, 'store'),  # Row 7
              ('1', 'L1', 'Y', KC2, KEY, '1'),
              ('2', 'L2', 'Z', KC2, KEY, '2'),
              ('3', 'L3', '', KC2, KEY, '3'),
              ('+', 'MEM', '"', KC4, KEY, '+')],
-            [('Off', '', '', KC1, FUN, 'off'),  # Row8
+            [('Off', '', '', KC1, FUN, 'off'),  # Row 8
              ('0', '', '', KC2, KEY, '0'),
              ('.', ':', '', KC2, KEY, '.'),
              ('(-)', 'ANS', '?', KC2, FUN, 'neg'),
-             ('Enter', 'Entry', '', KC4, FUN, 'enter')]
-        ]
+             ('Enter', 'Entry', '', KC4, FUN, 'enter')]]
 
         self.display = Pmw.ScrolledText(self, hscrollmode='dynamic',
                                         vscrollmode='dynamic', hull_relief='sunken',
@@ -173,3 +176,6 @@ class Evaluator:
 
 
 Calculator().mainloop()
+
+
+
